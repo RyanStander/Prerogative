57df90b8c26c2ede239326e72b51a795e742b553
Ryan <stander.ryan@gmail.com>
2021-08-11 14:56:25 +0200

    New Model with extra weapon functionality

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -8,6 +8,11 @@ public class PlayerInventory : MonoBehaviour
 
     public WeaponItem rightWeapon, leftWeapon;
 
+    public WeaponItem unarmedWeapon;
+
+    public WeaponItem[] weaponsInRightHandSlots = new WeaponItem[1], weaponsInLeftHandSlots = new WeaponItem[1];
+
+    public int currentRightWeaponIndex = 0, currentLeftWeaponIndex=0;
     private void Awake()
     {
         weaponSlotManager = GetComponentInChildren<WeaponSlotManager>();
@@ -15,9 +20,37 @@ public class PlayerInventory : MonoBehaviour
 
     private void Start()
     {
+        rightWeapon = weaponsInRightHandSlots[currentRightWeaponIndex];
+        leftWeapon = weaponsInLeftHandSlots[currentLeftWeaponIndex];
+
         if (rightWeapon!=null)
             weaponSlotManager.LoadWeaponOnSlot(rightWeapon, false);
         if (leftWeapon != null)
             weaponSlotManager.LoadWeaponOnSlot(leftWeapon, true);
+    }
+
+    public void ChangeRightWeapon()
+    {
+        currentRightWeaponIndex = currentRightWeaponIndex + 1;
+
+        for (int i = 0; i < weaponsInRightHandSlots.Length; i++)
+        {
+            if (currentRightWeaponIndex == i && weaponsInRightHandSlots[i] != null)
+            {
+                rightWeapon = weaponsInRightHandSlots[currentRightWeaponIndex];
+                weaponSlotManager.LoadWeaponOnSlot(weaponsInRightHandSlots[currentRightWeaponIndex], false);
+            }
+            else if (currentRightWeaponIndex == i && weaponsInRightHandSlots[i] == null)
+            {
+                currentRightWeaponIndex = currentRightWeaponIndex + 1;
+            }
+        }
+
+        if (currentRightWeaponIndex > weaponsInRightHandSlots.Length - 1)
+        {
+            currentRightWeaponIndex = -1;
+            rightWeapon = unarmedWeapon;
+            weaponSlotManager.LoadWeaponOnSlot(unarmedWeapon,false);
+        }
     }
 }
